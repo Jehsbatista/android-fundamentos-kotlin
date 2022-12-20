70c4c046fabaaa9f919771a3e325b8ca33eaf510
jessica.batista <jessica.batista@g.globo>
2022-12-20 18:20:58 -0300

    Variáveis mutáveis e imutáveis

---
+++ secao-5-kotlin-basico-conceitos-iniciais/kotlin/src/main/kotlin/VariaveisMutaveisEImutaveis.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    var nome = "tony"   // VAR = variável mutável, podendo modificar seu valor.
+    nome = "stark"
+
+    val sobrenome = "Batista"  //VAL = variável imutável, depois de atribuido um valor não poderá ser modificado.
+    sobrenome = "teixeira"
+
+}
