1701471ab1ddad1550a2bf76e316e8a197d50acd
jessica.batista <jessica.batista@g.globo>
2022-12-20 20:32:43 -0300

    Operadores Aritméticos

---
+++ secao-5-kotlin-basico-conceitos-iniciais/kotlin/src/main/kotlin/OperadoresAritmeticos.kt
@@ -0,0 +1,29 @@
+fun main() {
+    /*
+
+    -----operadores aritméticos------
+                + - * / %
+
+    */
+
+    var idade = 10
+    idade += 1    //soma
+    idade -= 3    //subtração
+    idade *= 2    //multiplicaçào
+    idade /= 2    //divisão
+    println(idade) //resultado
+
+    var idadee = 10 % 3  //utilizando % pegamos o resto
+    println(idadee)
+
+
+    //Incrementando valores//
+
+    var incremento = 20
+    incremento++              //utilizado ++ depois da variável, acessa o valor primeiro e depois incrementa em 1, passa a valer 21.
+    println(incremento)
+
+    var incrementoo = 21
+    ++incrementoo             //utilizando ++ antes da variável, incrementa em 1 e depois acessa o valor, passa a valer 22.
+    println(incrementoo)
+}
