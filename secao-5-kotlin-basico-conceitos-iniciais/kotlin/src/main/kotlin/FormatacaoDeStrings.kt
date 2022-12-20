f6f38fdf3320a064d10222bfcc8262ea2d28c968
jessica.batista <jessica.batista@g.globo>
2022-12-20 19:27:01 -0300

    Formatação de Strings.

---
+++ secao-5-kotlin-basico-conceitos-iniciais/kotlin/src/main/kotlin/FormatacaoDeStrings.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    val nome = "Jéssica"
+    val sobrenome = "Batista"
+
+    println("Olá $nome, qual seu sobrenome? Meu sobrenome é $sobrenome .") //utilizando o $ podemos fazer a interpolação dos valores.
+
+    println("Quantas letras o nome $nome tem? O nome $nome tem ${nome.length} letras.") //utilizando a propriedade length podemos acessar o tamanho da string.
+}
