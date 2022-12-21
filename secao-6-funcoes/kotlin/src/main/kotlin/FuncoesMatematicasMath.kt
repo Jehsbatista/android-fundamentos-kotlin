b021cc3db336fe4f81a14d5711fb5ccbb3207f73
jessica.batista <jessica.batista@g.globo>
2022-12-21 16:45:01 -0300

    Funções matemáticas - math

---
+++ secao-6-funcoes/kotlin/src/main/kotlin/FuncoesMatematicasMath.kt
@@ -0,0 +1,10 @@
+import kotlin.math.*
+//importando funcionalidades da biblioteca math
+fun main() {
+    println(max(10, 20))         //pega o valor máximo
+    println(min(15, 20))         //pega o valor mínimo
+    println(PI)                        // valor de PI
+    println(E)                         //valor de E
+    println(sqrt(9.74625))          //raiz quadrada de um float ou double
+    println(round(7.74294724749))   //arredonda números quebrados
+}
