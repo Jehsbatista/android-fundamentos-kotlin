0cf392e371c27ef2ac51e1a9e5d632542c0459d6
jessica.batista <jessica.batista@g.globo>
2022-12-21 20:26:00 -0300

    Controle de fluxo If

---
+++ secao-7-controle-de-fluxo/kotlin/src/main/kotlin/ControleDeFluxoIf.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    // na estrutura if só é executado se a condição for verdadeira
+
+    val num = 40
+
+    if (num > 30) {
+        println("verdadeiro")
+    }
+
+    if (num < 10) {
+        println("falso")
+    }
+}
