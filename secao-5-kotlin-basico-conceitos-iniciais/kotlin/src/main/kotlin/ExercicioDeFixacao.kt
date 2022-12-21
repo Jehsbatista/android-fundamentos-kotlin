14f48c03cecc6b54c6133b2cec8a1dce66870336
jessica.batista <jessica.batista@g.globo>
2022-12-20 21:05:57 -0300

    Exercício de Fixação seção 5

---
+++ secao-5-kotlin-basico-conceitos-iniciais/kotlin/src/main/kotlin/ExercicioDeFixacao.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    var nome = "Jéssica Batista"    //var capaz de armazenar meu nome
+    var texto: String = ""          // var de texto sem valor
+
+    val numeroCalcado: Byte = 36    //val imutável com menor tipo de dado para armazenar quanto eu calço
+    val pib = 1869000000000         //var capaz de armazenar o PIB do Brasil
+    val populacao = 211000000       //var capaz de armazenar a população do Brasil
+
+    println("pib per capita =  ${pib / populacao}")  //imprimindo o valor do PIB per capita
+}
+
