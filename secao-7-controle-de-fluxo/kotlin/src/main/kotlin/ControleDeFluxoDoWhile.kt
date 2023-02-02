7a960fd59a567a5b1f583963b2ebc75da5b9f0c7
jessica.batista <jessica.batista@g.globo>
2023-02-01 21:57:39 -0300

    Controle de Fluxo Do While

---
+++ secao-7-controle-de-fluxo/kotlin/src/main/kotlin/ControleDeFluxoDoWhile.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    // faça alguma coisa enquanto a condição for satisfeita
+    do {
+        println("Qual seu nome?")
+        var value = readln()
+    } while (value == "")
+}
+
