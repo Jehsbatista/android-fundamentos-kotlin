7805ca52d7e397d62079bacebb383e28aa123695
jessica.batista <jessica.batista@g.globo>
2023-02-01 21:46:50 -0300

    Controle de Fluxo While

---
+++ secao-7-controle-de-fluxo/kotlin/src/main/kotlin/ControleDeFluxoWhile.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    //É executado enquanto uma condição for satisfeita
+    var i = 0
+    while (i <= 100) {
+        println(i)
+        i++
+
+    }
+    //Utilizando uma string
+    val str = "Kotlin"
+    var i = 0
+    while (i < str.length) {
+        println("${str[i]}")
+        i++
+    }
+}
