138ac6258fcc7c3142ef4e2c4c3d900e676cf429
jessica.batista <jessica.batista@g.globo>
2023-04-10 21:44:56 -0300

    Anotation fail

--- secao-9-testes-unitarios/Kotlin/TestesUnitarios/src/test/kotlin/MainTest.kt
+++ secao-9-testes-unitarios/Kotlin/TestesUnitarios/src/test/kotlin/MainTest.kt
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import kotlin.test.fail
 
 class MainTest {
 
@@ -24,5 +25,9 @@ class MainTest {
     @Disabled
     fun naoImplementado() {
 
+    }
+    @Test
+    fun vaiFalhar() {
+        fail("Não posso terminar os testes sem esse método")
     }
 }
