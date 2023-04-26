b2e8eed2e3ed541e84738b50c42ad8f5900a921d
jessica.batista <jessica.batista@g.globo>
2023-04-25 21:57:48 -0300

    Manipulando conjunto de valores utilizando o Set

--- secao-12-colecoes-e-funcoes/Kotlin/src/main/kotlin/Main.kt
+++ secao-12-colecoes-e-funcoes/Kotlin/src/main/kotlin/Main.kt
@@ -1,18 +1,15 @@
-//collections - conjunto de elementos - List, Set e Map
 
 fun main() {
-    //List
-    val lista1: List<Int> = listOf(1, 2, 3, 4, 5, 6)
+//Set - coleção de elementos não ordenada, não permite valores duplicados
 
+    val set1: Set<String> = setOf<String>("São Paulo", "Rio de Jandeiro", "Minas Gerais", "Minas Gerais")
+    val set2: MutableSet<String> = mutableSetOf()
 
-    //mutableList
-    var lista2: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6)
+    println(set1)
 
-    println(lista2[2])
-    println(lista2.size)
-    lista2.add(8)
-    lista2.remove(1)
-    lista2.removeAt(1)
-    println(lista2.contains(5))
-    println(lista2)
+    set2.addAll(mutableListOf("Jorge", "João", "Carlos"))
+    set2.remove("Jorge")
+    println(set2.contains("Roberto"))
+    println(set2)
+
 }
